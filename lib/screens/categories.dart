c853d5ded5eab3a9c58e2b576bb16c822e48f0e0
akbar.daulay <akbar.daulay@quadrant-si.id>
2024-09-10 09:54:50 +0700

    adding card list item

--- lib/screens/categories.dart
+++ lib/screens/categories.dart
@@ -2,14 +2,17 @@ import 'package:flutter/material.dart';
 import 'package:meals_app/data/dummy_data.dart';
 import 'package:meals_app/screens/meals.dart';
 import 'package:meals_app/widgets/category_grid_item.dart';
+import 'package:meals_app/models/category.dart';
 
 class CategoriesScreen extends StatelessWidget {
   const CategoriesScreen({super.key});
 
-  void _selectCategory(BuildContext context) {
+  void _selectCategory(BuildContext context, Category category) {
+    final filteredMeals = dummyMeals.where((meal)=>meal.categories.contains(category.id)).toList();
+
     Navigator.of(context).push(MaterialPageRoute(
         builder: (ctx) =>
-            MealsScreen(title: 'Available Categories', meals: [])));
+            MealsScreen(title: category.title, meals: filteredMeals)));
   }
 
   @override
@@ -30,7 +33,7 @@ class CategoriesScreen extends StatelessWidget {
             // ...availableCategories.map((category) => CategoryGridItem(category: category)).toList(),
             for (final category in availableCategories)
               CategoryGridItem(category: category, onSelectCategory: (){
-                _selectCategory(context);
+                _selectCategory(context,category);
               } ,),
           ]),
     );
