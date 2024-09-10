ca53b62c71460ac07a38dd51bc26d8cd7fb55b14
akbar.daulay <akbar.daulay@quadrant-si.id>
2024-09-10 14:39:46 +0700

    adding favorites meals and removing favorites meals

--- lib/screens/meal_details.dart
+++ lib/screens/meal_details.dart
@@ -2,15 +2,25 @@ import 'package:flutter/material.dart';
 import 'package:meals_app/models/meal.dart';
 
 class MealDetailsScreen extends StatelessWidget {
-  const MealDetailsScreen({super.key, required this.meal});
+  const MealDetailsScreen(
+      {super.key, required this.meal, required this.onToggleFavorite});
 
   final Meal meal;
+  final void Function(Meal meal) onToggleFavorite;
 
   @override
   Widget build(BuildContext context) {
     return Scaffold(
       appBar: AppBar(
         title: Text(meal.title),
+        actions: [
+          IconButton(
+            onPressed: () {
+              onToggleFavorite(meal);
+            },
+            icon: const Icon(Icons.star),
+          )
+        ],
       ),
       body: SingleChildScrollView(
         child: Column(
@@ -21,28 +31,52 @@ class MealDetailsScreen extends StatelessWidget {
               width: double.infinity,
               fit: BoxFit.cover,
             ),
-            const SizedBox(height: 14,),
-            Text('Ingredients', style: Theme.of(context).textTheme.titleLarge!.copyWith(
-              color: Theme.of(context).colorScheme.primary,
-              fontWeight: FontWeight.bold,
-            ),),
-            const SizedBox(height: 14,),
-            for(final ingredient in meal.ingredients)
-              Text(ingredient, style: Theme.of(context).textTheme.bodyMedium!.copyWith(
-              color: Theme.of(context).colorScheme.onSurface,),),
-            const SizedBox(height: 24,),
-             Text('Steps', style: Theme.of(context).textTheme.titleLarge!.copyWith(
-              color: Theme.of(context).colorScheme.primary,
-              fontWeight: FontWeight.bold,
-            ),),
-            const SizedBox(height: 14,),
-               for(final steps in meal.steps)
-              Padding(
-                padding: const EdgeInsets.symmetric(horizontal: 12,vertical: 8,),
-                child: Text(steps, 
-                textAlign: TextAlign.center,
+            const SizedBox(
+              height: 14,
+            ),
+            Text(
+              'Ingredients',
+              style: Theme.of(context).textTheme.titleLarge!.copyWith(
+                    color: Theme.of(context).colorScheme.primary,
+                    fontWeight: FontWeight.bold,
+                  ),
+            ),
+            const SizedBox(
+              height: 14,
+            ),
+            for (final ingredient in meal.ingredients)
+              Text(
+                ingredient,
                 style: Theme.of(context).textTheme.bodyMedium!.copyWith(
-                color: Theme.of(context).colorScheme.onSurface,),),
+                      color: Theme.of(context).colorScheme.onSurface,
+                    ),
+              ),
+            const SizedBox(
+              height: 24,
+            ),
+            Text(
+              'Steps',
+              style: Theme.of(context).textTheme.titleLarge!.copyWith(
+                    color: Theme.of(context).colorScheme.primary,
+                    fontWeight: FontWeight.bold,
+                  ),
+            ),
+            const SizedBox(
+              height: 14,
+            ),
+            for (final steps in meal.steps)
+              Padding(
+                padding: const EdgeInsets.symmetric(
+                  horizontal: 12,
+                  vertical: 8,
+                ),
+                child: Text(
+                  steps,
+                  textAlign: TextAlign.center,
+                  style: Theme.of(context).textTheme.bodyMedium!.copyWith(
+                        color: Theme.of(context).colorScheme.onSurface,
+                      ),
+                ),
               ),
           ],
         ),
--- lib/screens/tabs.dart
+++ lib/screens/tabs.dart
@@ -1,9 +1,10 @@
 import 'package:flutter/material.dart';
+import 'package:meals_app/models/meal.dart';
 import 'package:meals_app/screens/categories.dart';
 import 'package:meals_app/screens/meals.dart';
 
 class TabsScreen extends StatefulWidget {
-  const TabsScreen({super.key});  
+  const TabsScreen({super.key});
 
   @override
   State<TabsScreen> createState() {
@@ -11,9 +12,34 @@ class TabsScreen extends StatefulWidget {
   }
 }
 
-
 class _TabsScreenState extends State<TabsScreen> {
   int _selectedPageIndex = 0;
+  final List<Meal> _favoriteMeals = [];
+
+  void _showInfoMessage(String message) {
+    ScaffoldMessenger.of(context).clearSnackBars();
+    ScaffoldMessenger.of(context).showSnackBar(
+      SnackBar(
+      
+        content: Text(message),
+      ),
+    );
+  }
+
+  void _toggleMealFavoriteStatus(Meal meal) {
+    final isExisting = _favoriteMeals.contains(meal);
+    if (isExisting) {
+      setState(() {
+      _favoriteMeals.remove(meal);
+      });
+      _showInfoMessage('${meal.title} was removed from favorites.');
+    } else {
+      setState(() {
+      _favoriteMeals.add(meal);
+      });
+      _showInfoMessage('${meal.title} was added to favorites.');
+    }
+  }
 
   void _selectPage(int index) {
     setState(() {
@@ -23,17 +49,22 @@ class _TabsScreenState extends State<TabsScreen> {
 
   @override
   Widget build(BuildContext context) {
-   Widget activePage = const CategoriesScreen();
-   var activePageTitle = 'Categories';
+    Widget activePage = CategoriesScreen(
+      onToggleFavorite: _toggleMealFavoriteStatus,
+    );
+    var activePageTitle = 'Categories';
 
-  if(_selectedPageIndex == 1){
-    activePage = const MealsScreen(meals:[]);
-    activePageTitle = 'Favorites';
-  }
+    if (_selectedPageIndex == 1) {
+      activePage = MealsScreen(
+        meals: _favoriteMeals,
+        onToggleFavorite: _toggleMealFavoriteStatus,
+      );
+      activePageTitle = 'Favorites';
+    }
 
     return Scaffold(
       appBar: AppBar(
-        title:  Text(activePageTitle),
+        title: Text(activePageTitle),
       ),
       body: activePage,
       bottomNavigationBar: BottomNavigationBar(
@@ -48,7 +79,6 @@ class _TabsScreenState extends State<TabsScreen> {
             icon: Icon(Icons.star),
             label: 'Favorites',
           ),
-
         ],
       ),
     );
