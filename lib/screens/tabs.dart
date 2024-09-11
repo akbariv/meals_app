bfd72d9e8e96eac0b03cc49cdf8c30f421484635
akbar.daulay <akbar.daulay@quadrant-si.id>
2024-09-11 10:53:00 +0700

    adding Drawer and filter screen

--- lib/screens/tabs.dart
+++ lib/screens/tabs.dart
@@ -1,7 +1,9 @@
 import 'package:flutter/material.dart';
 import 'package:meals_app/models/meal.dart';
 import 'package:meals_app/screens/categories.dart';
+import 'package:meals_app/screens/filters.dart';
 import 'package:meals_app/screens/meals.dart';
+import 'package:meals_app/widgets/main_drawer.dart';
 
 class TabsScreen extends StatefulWidget {
   const TabsScreen({super.key});
@@ -20,7 +22,6 @@ class _TabsScreenState extends State<TabsScreen> {
     ScaffoldMessenger.of(context).clearSnackBars();
     ScaffoldMessenger.of(context).showSnackBar(
       SnackBar(
-      
         content: Text(message),
       ),
     );
@@ -30,12 +31,12 @@ class _TabsScreenState extends State<TabsScreen> {
     final isExisting = _favoriteMeals.contains(meal);
     if (isExisting) {
       setState(() {
-      _favoriteMeals.remove(meal);
+        _favoriteMeals.remove(meal);
       });
       _showInfoMessage('${meal.title} was removed from favorites.');
     } else {
       setState(() {
-      _favoriteMeals.add(meal);
+        _favoriteMeals.add(meal);
       });
       _showInfoMessage('${meal.title} was added to favorites.');
     }
@@ -45,6 +46,14 @@ class _TabsScreenState extends State<TabsScreen> {
     setState(() {
       _selectedPageIndex = index;
     });
+  }
+
+  void _setScreen(String identifier) {
+    if (identifier == 'filters') {
+      Navigator.of(context).push(MaterialPageRoute(
+        builder: (ctx) => const FiltersScreen(),
+      ));
+    }
   }
 
   @override
@@ -65,6 +74,9 @@ class _TabsScreenState extends State<TabsScreen> {
     return Scaffold(
       appBar: AppBar(
         title: Text(activePageTitle),
+      ),
+      drawer: MainDrawer(
+        onSelectScreen: _setScreen,
       ),
       body: activePage,
       bottomNavigationBar: BottomNavigationBar(
